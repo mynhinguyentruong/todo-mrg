003d46b2ef35f5116e3b7d202bd173ee46b84987
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-08-01 11:00:37 -0400

    new migration and data

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { todos, users } from "./schema/schema";
+import { todos, users, todolists } from "./schema/schema";
 import * as schema from "./schema/schema";
 import { InferModel, eq } from "drizzle-orm";
 
@@ -16,9 +16,16 @@ const getUser = async () => {
   return results;
 };
 
-type NewTodo = InferModel<typeof todos, "insert">;
+export type NewTodo = InferModel<typeof todos, "insert">;
+export type Todo = InferModel<typeof todos>;
 
-const todo = {
+export type TodoList = InferModel<typeof todolists>;
+export type NewTodoList = InferModel<typeof todolists, "insert">;
+
+export type Users = InferModel<typeof users>;
+export type NewUser = InferModel<typeof users, "insert">;
+
+const todo: NewTodo = {
   authorId: 1,
   listId: 1,
   title: "Todo 1",
@@ -34,14 +41,38 @@ const addTodo = async (todo: NewTodo) => {
   await db.insert(todos).values(todo);
 };
 
-export const getAllTodos = async (userId: number) => {
+export const addTodoList = async ({ title, authorId }: NewTodoList) => {
   const result = await db
-    .select()
-    .from(todos)
-    .where(eq(todos.authorId, userId));
+    .insert(todolists)
+    .values({ title, authorId })
+    .returning({ id: todolists.id });
+
+  return result[0];
+};
+
+export const getAllTodos = async (listId: number) => {
+  const result = await db.select().from(todos).where(eq(todos.listId, listId));
   console.log({ result });
 
   return result;
+};
+
+export const getAllTodoLists = async (userId: number) => {
+  const result = await db
+    .select()
+    .from(todolists)
+    .where(eq(todolists.authorId, userId));
+
+  return result;
+};
+
+export const getATodoList = async (listId: number) => {
+  const result = await db
+    .select()
+    .from(todolists)
+    .where(eq(todolists.id, listId));
+
+  return result[0];
 };
 
 // getUser().catch((err) => console.log({ err }));
