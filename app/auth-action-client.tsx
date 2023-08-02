dfbf4c5ed3128f96effb408be046baba6b69e420
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-08-02 11:29:19 -0400

    client component for auth

--- app/auth-action-client.tsx
+++ app/auth-action-client.tsx
@@ -1,10 +1,11 @@
 'use client'
+import { signIn, signOut } from 'next-auth/react';
 
 export function SignOut() {
     return (
         <button
           className="text-xs text-neutral-700 dark:text-neutral-300 mt-2 mb-6"
-          onClick={() => console.log("Sign out")}
+          onClick={() => signOut()}
         >
           → Sign out
         </button>
@@ -15,7 +16,7 @@ export function SignIn() {
     return (
         <button
         className="flex bg-black text-neutral-200 px-4 py-3 rounded-md font-semibold text-sm mb-4 hover:text-white transition-all border border-gray-800"
-        onClick={() => console.log("Signing in")}
+        onClick={() => signIn('github')}
       >
         <GitHubIcon />
         <div className="ml-3">Sign in with GitHub</div>
